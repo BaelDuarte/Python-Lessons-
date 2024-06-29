24dc9eff1f6b752c0bac3550c90bacf810006f8f
BaelDuarte <baelduarte16@gmail.com>
2024-06-29 00:04:10 -0300

    twov1

---
+++ two.py
@@ -0,0 +1,5 @@
+#make a program that sums two numbers
+first_number = int(input('enter the fist number: '))
+second_number = int(input('enter the second number: '))
+total = first_number + second_number
+print("The sum result is " + {} + "!".format(total))
